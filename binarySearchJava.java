65f804c399e044b17a9a5416c898c3d4ded001d9
Mohammed Abdul Shanawaz <147296802+imrose05@users.noreply.github.com>
2025-01-16 23:07:58 +0530

    Add files via upload

---
+++ binarySearchJava.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class binarySearchJava {
+    
+    public static int binarySearch(int arr[], int target) {
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+        System.out.print("Enter the target element you want to search: ");
+        int target = sc.nextInt();
+        int result = binarySearch(arr, target);
+        if (result == -1) {
+            System.out.println("Element not found.");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+        sc.close();
+    }
+}
